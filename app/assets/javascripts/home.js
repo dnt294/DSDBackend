f017fc7fcf29fe822f3fba964dfb0efa8f0aef45
NTD <kaanoo2904@gmail.com>
2016-10-13 14:16:40 +0700

    add folders link to welcome page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,14 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+    // fix menu when passed
+    $('.welcome-menu').visibility({
+        once: false,
+        onBottomPassed: function() {
+            $('.following-header').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+            $('.following-header').transition('fade out');
+        }
+    });
+
+});
