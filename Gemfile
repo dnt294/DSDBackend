00123f242245f5f9fc24e4bfec03ca8afa5014d2
NTD <kaanoo2904@gmail.com>
2016-10-15 13:47:38 +0700

    add redis gem

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'typescript-rails'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
-
+gem 'redis'
 gem 'ancestry'
 
 gem 'simple_form'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
     # Code is not reloaded between requests.
     config.cache_classes = true
 
-    config.web_socket_server_url = "http://dsd14.herokuapp.com/cable"
+    config.web_socket_server_url = "ws://dsd14.herokuapp.com/cable"
     config.action_cable.allowed_request_origins = ['https://dsd14.herokuapp.com', 'http://adsd14.herokuapp.com']
 
     # Eager load code on boot. This eager loads most of Rails and
