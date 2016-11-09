ea994c7bb914ea43efc0fdca4ce869c2aecc7fd1
NTD <kaanoo2904@gmail.com>
2016-11-09 20:51:11 +0700

    config to run in linux virtual server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,6 @@ Rails.application.configure do
     # Code is not reloaded between requests.
     config.cache_classes = true
 
-    #config.web_socket_server_url = "wss://dsd14.herokuapp.com/cable"
-    #config.action_cable.allowed_request_origins = ['https://dsd14.herokuapp.com', /http:\/\/dsd14.herokuapp.com*/ ]
-
     # Eager load code on boot. This eager loads most of Rails and
     # your application in memory, allowing both threaded web servers
     # and those relying on copy on write to perform better.
