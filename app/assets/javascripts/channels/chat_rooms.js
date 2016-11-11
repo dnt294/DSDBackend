c14244ce1f93ccccb07aa7fad481663122e48247
NTD <kaanoo2904@gmail.com>
2016-11-11 22:54:13 +0700

    broadcast to seperated chat room when send message

---
+++ app/assets/javascripts/channels/chat_rooms.js
@@ -0,0 +1,25 @@
+$(document).on('turbolinks:load', function() {
+    App.chat_rooms = App.cable.subscriptions.create({
+
+        channel: 'ChatRoomsChannel',
+        chat_room_id: $('.comment-list').data('chat-room-id')
+    }, {
+        // main socket function
+        connected: function() {
+
+        },
+
+        disconnected: function() {
+
+        },
+
+        received: function(data) {
+            return $('#comments').prepend(this.renderMessage(data));
+        },
+
+        // extra function
+        renderMessage: function(data) {
+            return "<p> <b>" + data.user + ": </b>" + data.comment + "</p>";
+        }
+    });
+});
--- app/jobs/comments_broadcast_job.rb
+++ app/jobs/comments_broadcast_job.rb
@@ -1,8 +1,8 @@
 class CommentsBroadcastJob < ApplicationJob
     queue_as :default
 
-    def perform(comment)
-    	ActionCable.server.broadcast 'comments',
+    def perform(comment)    	
+    	ActionCable.server.broadcast "chat_rooms_#{comment.chat_room.id}",
                 comment: comment.content,
                 user: comment.user.username
             head :ok
