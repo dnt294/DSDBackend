9ab10f4159cde5f696c9d8ee722dd25811d3de1a
NTD <kaanoo2904@gmail.com>
2016-11-12 18:01:12 +0700

    limit to 100MB when upload files

--- app/assets/javascripts/folders.js
+++ app/assets/javascripts/folders.js
@@ -44,8 +44,8 @@ $(document).on('turbolinks:load', function() {
 
     $('#new_up_file').fileupload({
         dataType: 'script',
-        maxChunkSize: 5000000,
-        maxFileSize: 5000000 * 30,
+        maxChunkSize: 5000000, // upload 5MB 1 lần
+        maxFileSize: 5000000 * 20, // to nhất là 100MB
         progress: function(e, data) {
 
             var complete_percent = parseInt(data.loaded / data.total * 100, 10);
@@ -55,9 +55,14 @@ $(document).on('turbolinks:load', function() {
             types = /(\.|\?)(jpg|JPG|gif|GIF|mp4|MP4|mp3|MP3|pdf|PDF)$/i
             file = data.files[0];
             if (types.test(file.type) || types.test(file.name)) {
-                data.context = $(tmpl("template-upload", data.files[0]));
-                $('#new_up_file').append(data.context);
-                data.submit();
+
+                if (file.size > 100000000) {
+                    alert('Quá kích thước cho phép 100MB.');
+                } else {
+                    data.context = $(tmpl("template-upload", data.files[0]));
+                    $('#new_up_file').append(data.context);
+                    data.submit();
+                }
             } else {
                 alert(file.type + " không được hỗ trợ.");
             }
